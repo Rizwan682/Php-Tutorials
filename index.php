be03c753eb7b6602ef023730846dbcf0bfd412cf
Rizwan682 <146113408+Rizwan682@users.noreply.github.com>
2023-10-01 08:21:23 -0700

    changed document title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> PHP BASIC PROGRAM</title>
 </head>
 <body>
 <h1> <?php  echo "$message" ?></h1>
